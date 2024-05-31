500507aaab1b47433a731a9ff88e3b7bca6d229f
Kenneth Sequeira <seq.kenneth@gmail.com>
2024-05-31 09:05:45 +0530

    add keys through os.environ

--- voicebot2.py
+++ voicebot2.py
@@ -5,6 +5,7 @@ from twilio.twiml.voice_response import VoiceResponse
 from azure.cognitiveservices.speech import SpeechConfig, AudioConfig, SpeechRecognizer, SpeechSynthesizer, SpeechSynthesisOutputFormat
 import openai
 import logging
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -15,7 +16,7 @@ logging.basicConfig(filename='voicebot.log', level=logging.INFO)
 # Initialize Microsoft Speech SDK
 def initialize_speech_sdk():
     try:
-        subscription_key = "7067854f22dd47ec875340ecc0e4e813"
+        subscription_key = os.environ['AZURE_SPEECH_KEY']
         region = "centralindia"
 
         speech_config = SpeechConfig(subscription=subscription_key, region=region)
@@ -33,14 +34,14 @@ speech_recognizer, speech_synthesizer = initialize_speech_sdk()
 # Initialize GPT-4O
 def initialize_openai():
     try:
-        openai.api_key = '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'
+        openai.api_key = os.environ['OPENAI_KEY']
     except Exception as e:
         logging.error(f"Error initializing OpenAI: {e}")
 
 initialize_openai()
 
 # Define route for handling incoming calls
-@app.route("/voicebot2", methods=['POST'])
+@app.route("/voicebot2", methods=['GET','POST'])
 def voicebot():
     try:
         # Get the speech input from the incoming call
